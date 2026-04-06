89387b1f4dc0b30607b1ce3c52202fc8a9d837a6
raphdcst <rdecostaire@gmail.com>
2026-04-06 17:10:37 +0200

    feat(ui): modify default layout and homepage to use @nuxt/ui

--- inertia/layouts/default.vue
+++ inertia/layouts/default.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
-import { watch } from 'vue'
-import { usePage } from '@inertiajs/vue3'
-import { toast, Toaster } from 'vue-sonner'
-import type { Data } from '@generated/data'
-import { Link, Form } from '@adonisjs/inertia/vue'
+import type { Data } from '@generated/data';
+import { usePage } from '@inertiajs/vue3';
+
+import { watch } from 'vue';
 
 const page = usePage<Data.SharedProps>()
 
+const toast = useToast()
+
 watch(
   () => page.url,
-  () => toast.dismiss()
+  () => toast.clear()
 )
 
 watch(
   () => page.props.flash,
   (flashMessages) => {
     if (flashMessages.error) {
-      toast.error(flashMessages.error)
+      toast.add({
+        color: 'error',
+        title: flashMessages.error
+      })
     }
     if (flashMessages.success) {
-      toast.success(flashMessages.success)
+      toast.add({
+        color: 'success',
+        title: flashMessages.success
+      })
     }
   },
   { immediate: true }
@@ -27,44 +34,9 @@ watch(
 </script>
 
 <template>
-  <header>
-    <div>
-      <div>
-        <Link route="home">
-          <svg
-            width="66"
-            height="24"
-            viewBox="0 0 105 38"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M0 0h7.5v15H0ZM7.5 15h7.5v15H7.5ZM15 30h7.5v7.5H15ZM22.5 15h7.5v15H22.5ZM30 0h7.5v15H30ZM45 0h7.5v30h15v-30h7.5v37.5h-30v-37.5ZM82.5 37.5V0H105v7.5H90V15h15v7.5H90V30h15v7.5H82.5Z"
-              fill="currentColor"
-            />
-          </svg>
-        </Link>
-      </div>
-      <div>
-        <nav>
-          <template v-if="page.props.user">
-            <span>{{ page.props.user.initials }}</span>
-            <Form route="session.destroy">
-              <button type="submit">Logout</button>
-            </Form>
-          </template>
-          <template v-else>
-            <Link route="new_account.create">Signup</Link>
-            <Link route="session.create">Login</Link>
-          </template>
-        </nav>
-      </div>
-    </div>
-  </header>
-
-  <main>
-    <slot />
-  </main>
-
-  <Toaster position="top-center" rich-colors />
+  <UApp>
+    <UMain>
+      <slot />
+    </UMain>
+  </UApp>
 </template>
