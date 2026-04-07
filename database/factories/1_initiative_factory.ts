b4e6b59ab858fa829c0abf68df7eee3f672110ec
raphdcst <rdecostaire@gmail.com>
2026-04-07 23:59:20 +0200

    feat(db): add fake date generation to initiative factory

--- database/factories/1_initiative_factory.ts
+++ database/factories/1_initiative_factory.ts
@@ -1,11 +1,13 @@
 import Initiative from '#models/initiative'
 import factory from '@adonisjs/lucid/factories'
+import { DateTime } from 'luxon'
 
 export const InitiativeFactory = factory
   .define(Initiative, async ({ faker }) => {
     return {
       name: faker.commerce.productName(),
       description: faker.commerce.productDescription(),
+      createdAt: DateTime.fromISO(faker.date.recent({ days: 10 }).toISOString()),
     }
   })
   .build()
