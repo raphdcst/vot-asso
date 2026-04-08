c162bd87a3027d2a389b0f1de99f5f39fa430fc6
raphdcst <rdecostaire@gmail.com>
2026-04-08 10:18:12 +0200

    fix(server): modify static server config to enable assets caching

--- config/static.ts
+++ config/static.ts
@@ -8,40 +8,13 @@ import { defineConfig } from '@adonisjs/static'
  * https://docs.adonisjs.com/guides/basics/static-file-server
  */
 const staticServerConfig = defineConfig({
-  /**
-   * Enable or disable static file serving middleware.
-   */
   enabled: true,
-
-  /**
-   * Generate ETag headers for client/proxy caching.
-   */
   etag: true,
-
-  /**
-   * Include Last-Modified headers for conditional requests.
-   */
   lastModified: true,
-
-  /**
-   * Policy for files starting with a dot.
-   */
   dotFiles: 'ignore',
-
-  /**
-   * Enable caching for assets
-   */
-  headers: (path) => {
-    if (path.startsWith('assets/')) {
-      return {
-        'Cache-Control': 'public, max-age=31536000, immutable',
-      }
-    }
-
-    return {
-      'Cache-Control': 'public, max-age=3600',
-    }
-  },
+  cacheControl: true,
+  maxAge: '1 year',
+  immutable: true,
 })
 
 export default staticServerConfig
